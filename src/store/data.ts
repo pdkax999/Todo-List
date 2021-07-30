59562c915175edc73d0eba58c18df8b9e0efaaa2
dream <10683954@qq.com>
2021-07-30 09:48:12 +0800

    预览

--- src/store/data.ts
+++
@@ -1,5 +0,0 @@
-const now = new Date();
-export default {
-  today: new Date(now.getFullYear(), now.getMonth(), now.getDate()),
-  tomorrow: new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1),
-};
